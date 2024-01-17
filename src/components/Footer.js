26c289ac13348cf453f4e16904c1e71ed6f95d31
rabimorad <73141546+rabimorad@users.noreply.github.com>
2024-01-17 11:42:57 +0100

    Update Footer.js
    year update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#BF3475] flex justify-center items-center p-4 text-gray-300'>Rabi Morad - 2022</div>
+    <div className='bg-[#BF3475] flex justify-center items-center p-4 text-gray-300'>Rabi Morad - 2024</div>
   )
 }
 
